6e6345b7b66795c42057e9ece1505903aa4a413d
idontknowwhatthatis <77902299+idontknowwhatthatis@users.noreply.github.com>
2025-09-14 15:18:05 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'll-main': '#47c1ff'
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+}
