e25be20b4a19e3338a4eb3a54a13f47fdd1e9dd6
idontknowwhatthatis <77902299+idontknowwhatthatis@users.noreply.github.com>
2025-09-14 15:21:30 -0400

    Create episodes.js

---
+++ data/episodes.js
@@ -0,0 +1,10 @@
+// Small episodes DB (edit/add). sampleAudio should be public URLs (S3, Vercel Storage, or CDN)
+const EPISODES = [
+  {
+    id: '1',
+    title: 'What do we call it?',
+    description: 'We dropped a friend :D',
+    date: '2025-09-14',
+    duration: '1:18:54',
+    sampleAudio: '' // <-- paste a public mp3 URL
+  }
