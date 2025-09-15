22a68e8d822ee9d74d1872f936e942e096724847
idontknowwhatthatis <77902299+idontknowwhatthatis@users.noreply.github.com>
2025-09-14 22:38:31 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -70,8 +70,8 @@ export default function Home() {
             <iframe
   title="Lemon Lime Podcast Episode 1"
   src="https://open.spotify.com/embed/episode/6koPyVHIfFDnjApVBm0kOk?utm_source=generator"
-  width="200%"
-  height="232"   // 🔥 Taller, shows artwork & details
+  width="100%"
+  height="175"   // 🔥 Taller, shows artwork & details
   frameBorder="0"
   allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
   loading="lazy"
